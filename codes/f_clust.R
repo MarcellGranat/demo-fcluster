20a86f030ffaef1251af9e05f7a84e2dd48bda42
MarcellGranat <granatcellmar98@gmail.com>
2022-03-06 12:39:58 +0100

    least total distance

--- codes/f_clust.R
+++ codes/f_clust.R
@@ -29,39 +29,21 @@ center_2000_df <- center_df %>% # find the one most similar to 2000s clusters
 
 distance_df <- center_df %>% 
   # eucledian distance among clusters
-  filter(time != 2000) %>% 
   pivot_longer(-(1:2)) %>% 
   left_join(select(center_2000_df, - time), by = "name") %>% 
   group_by(time, clust, clust_2000) %>% 
   summarise(distance = sum((value-value_2000)^2)) # ^^
 
-proper_cluster_df <- distance_df %>% 
+proper_cluster_df <- combinations(3) %>% 
+  rename(clust = x, clust_2000 = y) %>% 
+  left_join(distance_df) %>% 
+  group_by(time, comb) %>% 
+  mutate(
+    total_distance = sum(distance)
+  ) %>% 
   group_by(time) %>% 
-  nest() %>% 
-  ungroup() %>% 
-  transmute(time, proper = map(data, function(x) {
-    
-    x_arranged <- x %>% 
-      arrange(distance) # least different on top…
-    
-    out <- tibble() # collector for loop
-    
-    while (T) {
-      out <- x_arranged %>% 
-        head(1) %>% # match the least different pair ^^
-        bind_rows(out)
-      
-      x_arranged <- x_arranged %>% 
-        anti_join(out[, 1]) %>% # remove the paired ones…
-        anti_join(out[, 2]) # …^
-      
-      if (nrow(x_arranged) == 0) break # stop sorting if all paired
-    }
-    out
-  })) %>% 
-  unnest() %>%
-  bind_rows(tibble(time = 2000, clust = 1:3, clust_2000 = 1:3, distance = 0)) %>% 
-  arrange(time, clust)
+  filter(total_distance == min(total_distance)) %>% 
+  arrange(time) 
 
 proper_cluster_v <- proper_cluster_df %>% 
   arrange(time, clust) %>% 
@@ -69,6 +51,8 @@ proper_cluster_v <- proper_cluster_df %>%
   select(clust_2000) %>% 
   nest(clust_2000 = clust_2000) %>% 
   mutate(clust_2000 = map(clust_2000, pull))
+
+pull(proper_cluster_v)
 
 u_df <- fit_df %>%
   filter(k == 3) %>% 
@@ -82,6 +66,7 @@ u_df <- fit_df %>%
   mutate(
     proper_u = pmap(list(clus_1, clus_2, clus_3, clust_2000), function(clus_1, clus_2, clus_3, clust_2000) {
       # cluster prop in correct order (to 2000s clusters) 
+      print(clust_2000)
       c(clus_1, clus_2, clus_3)[clust_2000] %>% # sorting vector
         enframe(name = NULL) %>% # to df…
         t() %>% 
@@ -96,7 +81,6 @@ h_df <- center_df %>%
   left_join(proper_cluster_df) %>% 
   select(- clust, - distance) %>% 
   select(time, clust = clust_2000, everything()) # cluster in correct order
-
 
 demog_descriptive_df <- demog_df %>% # re-scaling estimated H values…
   select(- country, - geo) %>% 
--- run.command
+++ run.command
@@ -1,5 +1,6 @@
-cd projects/demo-fcluster
+cd projects/demo-fcluster # location on my mac
 Rscript codes/utils.R
 Rscript codes/data_setup.R
 Rscript codes/f_clust.R
 Rscript codes/highest_diff.R
+r -e "rmarkdown::render('results.Rmd', output_file = 'results.md', output_format = 'github_document')"
